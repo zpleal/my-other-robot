6f417dac983559e428522f0cc6bbfbadefe385b1
José Paulo Leal <zp@dcc.fc.up.pt>
2023-01-31 18:18:40 +0000

    Initial commit

---
+++ src/man/MyOtherRobot.java
@@ -0,0 +1,19 @@
+package man;
+
+import robocode.Robot;
+import robocode.ScannedRobotEvent;
+
+public class MyOtherRobot extends Robot {
+    public void run() {
+        while (true) {
+            ahead(100);
+            turnGunRight(360);
+            back(100);
+            turnGunRight(360);
+        }
+    }
+
+    public void onScannedRobot(ScannedRobotEvent e) {
+        fire(1);
+    }
+}
